a5cf704ddda848f1cf6330e1d427ff734855794e
Alex Clark <150987548+abclar1@users.noreply.github.com>
2025-04-10 14:51:47 -0500

    Lights working and added comments

--- TEC284_Quiz2.py
+++ TEC284_Quiz2.py
@@ -1,5 +1,7 @@
+#import the LED and Button
 from gpiozero import LED, Button
 
+#define the pins for the buttons and LED
 redButton = Button(4)
 greenButton = Button(17)
 blueButton = Button(27)
@@ -7,6 +9,7 @@ redLight = LED(16)
 greenLight = LED(20)
 blueLight = LED(21)
 
+#define a fuction to control the lights
 def controlLights():
     if redButton.is_pressed:
         redLight.on()
@@ -23,7 +26,8 @@ def controlLights():
     else:
             blueLight.off()
             
-            
+#create a infinte loop then call the control lights function      
 while True:
     controlLights()
+
 
